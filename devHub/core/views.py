65cae37f23d2aaee26732bed3daf88c5c04a2b18
kwabenahcode <mrofori507@gmail.com>
2024-10-30 21:53:55 +0000

    adding messages theme

--- devHub/core/views.py
+++ devHub/core/views.py
@@ -63,7 +63,6 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
     participants = room.participants.all()
-    print(participants)
     if request.method == "POST":
         message = Message.objects.create(
             user = request.user,
@@ -71,6 +70,7 @@ def room(request, pk):
             body = request.POST.get('body')
         )
         room.participants.add(request.user)
+        print(participants)
         return redirect('room', pk=room.id)
     context = {"room":room, "room_messages":room_messages, 'participants':participants }
     print(context)
