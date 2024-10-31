6e6534e0e1d889ea25a0b0a16b200857d8509c5c
kwabenahcode <mrofori507@gmail.com>
2024-10-31 00:26:41 +0000

    added update room def

--- devHub/core/views.py
+++ devHub/core/views.py
@@ -95,17 +95,15 @@ def createRoom(request):
         Room.objects.create(
             host = request.user,
             topic = topic,
-            name = request.POST.get(''),
+            name = request.POST.get('name'),
             description=request.POST.get('description'),
-
         )
         return redirect('home')
         #form = RoomForm(request.POST)
         # if form.is_valid():
         #     room = form.save(commit=False)
         #     room.host = request.user
-        #     room.save()
-            
+        #     room.save()     
     context = {"form": form, "topics":topics}
     return render(request, 'core/room_form.html', context)
 
@@ -114,11 +112,14 @@ def updateRoom(request, pk):
     form = RoomForm(instance=room)
     topics = Topic.objects.all()
     if request.method =='POST':
-        if form.is_valid:
-            form = RoomForm(request.POST, instance=room, )
-            form.save()
-            return redirect('home')
-    context = {"form":form, "topics":topics}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {"form":form, "topics":topics, "room":room}
     return render(request, 'core/room_form.html', context)
 
 @login_required(login_url='login')
