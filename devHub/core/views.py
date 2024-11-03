20668987bf975c6d89438e462fceccc3de4425e0
kwabenahcode <mrofori507@gmail.com>
2024-11-03 14:14:13 +0000

    adding activity-mobile

--- devHub/core/views.py
+++ devHub/core/views.py
@@ -53,7 +53,7 @@ def Home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {"rooms":rooms, 'topics':topics, "room_count":room_count, "room_messages":room_messages}
